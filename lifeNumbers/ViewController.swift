c8a691285c2c03f95c09b31978e18714d61716d9
Yoga <jc051533@gmail.com>
2022-06-25 13:33:01 +0800

    Initial Commit

---
+++ lifeNumbers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifeNumbers
+//
+//  Created by 王鈞則 on 2022/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
